e793e3ad2d3649a4e863a1170be404ba21d9078b
Anurag Upadhyay <anuragle100@gmail.com>
2022-10-08 17:29:20 +0530

    added sql alchemy for database access

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
+import datetime
+import json
+import os
 from dotenv import load_dotenv
 from celery import Celery
 from flask import Flask
+from celery.schedules import crontab
+from datetime import datetime
+import logging
+import requests
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
+load_dotenv()
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQL_URI")
 app.config["CELERY_broker_url"] = os.getenv("REDIS_URL")
 app.config["result_expires"] = 30
 app.config["timezone"] = 'UTC'
@@ -14,3 +27,8 @@ app.config["beat_schedule"] = {
 }
 celery = Celery(app.name, broker=app.config["CELERY_broker_url"])
 celery.conf.update(app.config)
+
+db = SQLAlchemy()
+db.init_app(app)
+with app.app_context():
+    db.create_all()
