f8e3688208bb913d6ba3ffdad6b8ce1153cbc6a9
Anurag Upadhyay <anuragle100@gmail.com>
2022-10-08 17:27:50 +0530

    added celery config and added task schedule

--- app.py
+++ app.py
@@ -3,5 +3,14 @@ from celery import Celery
 from flask import Flask
 app = Flask(__name__)
 app.config["CELERY_broker_url"] = os.getenv("REDIS_URL")
+app.config["result_expires"] = 30
+app.config["timezone"] = 'UTC'
+app.config["beat_schedule"] = {
+    'test-celery': {
+        'task': 'app.fetch_latest_videos',
+        # Every minute
+        'schedule': crontab(minute='*'),
+    }
+}
 celery = Celery(app.name, broker=app.config["CELERY_broker_url"])
 celery.conf.update(app.config)
