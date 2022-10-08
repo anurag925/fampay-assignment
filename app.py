247ab88db35ac26904a6e314c5ff7c7c8391a1ca
Anurag Upadhyay <anuragle100@gmail.com>
2022-10-08 17:26:49 +0530

    added celery

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
+from dotenv import load_dotenv
+from celery import Celery
 from flask import Flask
 app = Flask(__name__)
+app.config["CELERY_broker_url"] = os.getenv("REDIS_URL")
+celery = Celery(app.name, broker=app.config["CELERY_broker_url"])
+celery.conf.update(app.config)
